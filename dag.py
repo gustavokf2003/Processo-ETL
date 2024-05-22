c1883f8a2a101133d8a59be6bd6a0ead44cf7ce8
gustavokf2003 <gustavokf2003@gmail.com>
2024-05-22 12:08:48 -0300

    Mudança na extração dos dados

--- dag.py
+++ dag.py
@@ -3,6 +3,8 @@ from datetime import datetime
 import requests
 import zipfile
 import os
+import pandas as pd
+import json
 
 # Configuração do drag
 @dag(
@@ -10,13 +12,14 @@ import os
     start_date=datetime(2024, 5, 20),
     tags=['etl'],
     schedule='@monthly',
+    max_active_tasks=16,
+    concurrency=16
 )
 
 def etl():
     @task(task_id='download_e_extracao_zip')
-    def download_e_extracao_zip(url: str):
+    def download_e_extracao_zip(url: str, nome_pasta: str = 'dados'):
         arquivo_zip = 'arquivo.zip'
-        nome_pasta = 'dados'
         
         # Download do arquivo ZIP
         response = requests.get(url)
@@ -36,11 +39,45 @@ def etl():
         os.remove(arquivo_zip)
         print(f"Arquivo ZIP removido: {arquivo_zip}")
 
-    urls = ['https://drive.google.com/uc?export=download&id=14qBOhrE1gioVtuXgxkCJ9kCA8YtUGXKA', 
-            'https://drive.google.com/uc?export=download&id=1-caam_dahYOf2eorq4mez04Om6DD5d_3',
-            'https://drive.google.com/uc?export=download&id=1wskEgRC3ame7rncSDQ7qWhKsoKw1lohY']
+    @task(task_id='merge_dados')
+    def merge_dados(arquivos: list, ano: str):
+        # Fazer merge dos DataFrame com mesmo ano
+  
+        dfs = [pd.read_csv(f"dados/{arquivo}", delimiter=';', encoding='latin1') for arquivo in arquivos]
+        df = pd.merge(dfs[0][['id', 'tipo_veiculo', 'marca', 'ano_fabricacao_veiculo']], dfs[1], how='inner', on='id')
+        df = df.drop_duplicates()
+        df.to_csv(f'dados/resultado_{ano}.csv', index=False)
+
+        for arquivo in arquivos:
+            os.remove(f'dados/{arquivo}')
     
-    for url in urls:
-        download_e_extracao_zip(url)
+    # Leitura do arquivo json com as URLs
+    with open('urls.json', 'r') as json_file:
+        urls = json.load(json_file)
+            
+    # Tarefas de download e extração
+    download_tasks_2024_causas = download_e_extracao_zip(urls['2024']['causas']['link'])
+    download_tasks_2024_ocorrencia = download_e_extracao_zip(urls['2024']['ocorrencia']['link'])
+
+    download_tasks_2023_causas = download_e_extracao_zip(urls['2023']['causas']['link'])
+    download_tasks_2023_ocorrencia = download_e_extracao_zip(urls['2023']['ocorrencia']['link'])
+
+    download_tasks_2022_causas = download_e_extracao_zip(urls['2022']['causas']['link'])
+    download_tasks_2022_ocorrencia = download_e_extracao_zip(urls['2022']['ocorrencia']['link'])
+
+    # Tarefas de merge 
+    merge_dados_task_2024 = merge_dados([urls['2024']['causas']['nome'], urls['2024']['ocorrencia']['nome']], '2024')
+    merge_dados_task_2023 = merge_dados([urls['2023']['causas']['nome'], urls['2023']['ocorrencia']['nome']], '2023')
+    merge_dados_task_2022 = merge_dados([urls['2022']['causas']['nome'], urls['2022']['ocorrencia']['nome']], '2022')
+    
+    # Ordem das tarefas
+    download_tasks_2024_causas >> merge_dados_task_2024
+    download_tasks_2024_ocorrencia >> merge_dados_task_2024
+
+    download_tasks_2023_causas >> merge_dados_task_2023
+    download_tasks_2023_ocorrencia >> merge_dados_task_2023
+
+    download_tasks_2022_causas >> merge_dados_task_2022
+    download_tasks_2022_ocorrencia >> merge_dados_task_2022
 
 etl()
